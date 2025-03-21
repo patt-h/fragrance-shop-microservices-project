103cfcfbe619766ce2d92480781f2627e9d384f6
patt-h <patryk.huchla@wp.pl>
2025-03-21 18:28:29 +0100

    Removed Spring Open Feign from order-service

--- order-service/src/main/java/com/example/orderservice/Client/InventoryClient.java
+++ order-service/src/main/java/com/example/orderservice/Client/InventoryClient.java
@@ -1,13 +1,10 @@
 package com.example.orderservice.Client;
 
-import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.service.annotation.GetExchange;
 
-@FeignClient(value = "inventory", url = "http://localhost:8082")
 public interface InventoryClient {
 
-    @RequestMapping(method = RequestMethod.GET, value = "/api/inventory")
+    @GetExchange("/api/inventory")
     boolean isInStock(@RequestParam Long productId, @RequestParam Integer quantity);
 }
